d2b99a185bfc818a1f54489a9f7e21429a115cac
严立晨 <lichen.yan@daydaycook.com>
2018-12-12 11:14:07 +0800

    骨架屏修改

--- src/pages/offlineCourse/router.js
+++ src/pages/offlineCourse/router.js
@@ -3,7 +3,7 @@ const innerList = () => import('./innerList');
 
 const indexRouter = [
 	{ path: '/', name: 'indexList', component: indexList, meta: { c: 'offlineCoruse', a: 'index' } },
-	{ path: '/offlineCourse/innerList', name: 'innerList', component: innerList, meta: { c: 'offlineCoruse', a: 'inner' } },
+	{ path: '/inner', name: 'innerList', component: innerList, meta: { c: 'offlineCoruse', a: 'inner' } },
 ];
 
 export default indexRouter
